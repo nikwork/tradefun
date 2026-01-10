b7231ae7342f5a1e61764f703b0db4e1d4210217
Nikolai <vlasov.bis@gmail.com>
2026-01-10 15:41:26 +0300

    add simple env logging.

--- integrations/common/base_client.py
+++ integrations/common/base_client.py
@@ -3,6 +3,7 @@ import logging
 
 class BaseClient:
     def __init__(self) -> None:
+        logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(
             f"{__name__}.{self.__class__.__name__}",
         )
--- integrations/tbank/containers.py
+++ integrations/tbank/containers.py
@@ -7,8 +7,14 @@ from integrations.common.base_client import BaseClient
 from integrations.common.utils import get_bool_env_var
 
 
-class ApiClient:
+class ApiClient(BaseClient):
+    def _get_t_invest_env_info_msg(self) -> str:
+        invest_grpc_api_str = str(self._invest_grpc_api)
+        env_name = "SANDBOX" if self._is_sandbox else "PROD"
+        return f"Client for the env: {env_name} (target: {invest_grpc_api_str})."
+
     def __init__(self, api_key: str, is_sandbox: bool):
+        super().__init__()
         self._api_key = api_key
         self._is_sandbox = is_sandbox
         self._invest_grpc_api = (
@@ -17,15 +23,27 @@ class ApiClient:
         self._api_client = self.get_new_client()
 
     def get_new_client(self):
+        self.logger.info(f"Get new client ({self._get_t_invest_env_info_msg()})")
         return Client(self._api_key, target=self._invest_grpc_api)
 
     def get_client(self):
+        self.logger.info(f"Get client ({self._get_t_invest_env_info_msg()})")
         return self._api_client
 
 
 class CustomClient(BaseClient):
+
+    def _get_t_invest_env_info_msg(self) -> str:
+        invest_grpc_api_str = str(self.api_client._invest_grpc_api)
+        env_name = "SANDBOX" if self.api_client._is_sandbox else "PROD"
+        return f"Client for the env: {env_name} (target: {invest_grpc_api_str})."
+
     def __init__(self, api_client: ApiClient):
+        super().__init__()
         self.api_client = api_client
+        self.logger.info(
+            f"Init for the client id done ({self._get_t_invest_env_info_msg()})."
+        )
 
     def get_accounts(self):
         with self.api_client.get_new_client() as client:
